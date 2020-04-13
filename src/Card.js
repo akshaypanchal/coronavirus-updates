10f8a363e12e0444296679c1baf9c176239c701d
Akshay <akshaypanchal67@gmail.com>
2020-04-13 15:38:08 -0400

    deleting unneccessary Files and adding tachyons package for styling

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () =>{
+	return (
+
+	<div>
+		<img alt = "robots" src="https://robohash.org/test?200x200" />
+		<div>
+			<h2>Akshay Panchal</h2>
+			<p>akshaypanchal67@gmail.com</p>
+		</div>	
+	</div>	
+	);
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+  		<Card />
+  </div>,
   document.getElementById('root')
 );
 
